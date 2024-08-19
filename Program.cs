fc37af0c57881c4e0cea8f4d8a25c2dfffa292f2
Daniel Dutra <7233639+dandandandaann@users.noreply.github.com>
2024-08-19 16:26:27 +1000

    git init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace JonasBot;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hi, Jonas.");
+        // Console.WriteLine("Você mora dentro da baleia.");
+        Console.WriteLine("Whatever I write here will show up on screen.");
+
+        Console.WriteLine("Did you understand?");
+        var text = Console.ReadLine();
+
+        if(text == "yes")
+            Console.WriteLine("Well done.");
+        else 
+            Console.WriteLine("Wrong answer.");
+
+        Console.ReadKey();
+    }
+}
